c7897db3b3c2e6d93c9223d86c6da1208166f948
Andrea Pineda <andreapc@live.nl>
2017-11-02 12:01:58 +0100

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Andrea Pineda on 02/11/2017.
+//  Copyright © 2017 Andrea Pineda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
