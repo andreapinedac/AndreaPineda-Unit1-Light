d62c85ae69eb3925bdf4ef25692e01a88629215f
Andrea Pineda <andreapc@live.nl>
2017-11-03 12:00:39 +0100

    afronding

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,9 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var lightOn = true
+    
+    @IBOutlet weak var lightButton: UIButton!
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
